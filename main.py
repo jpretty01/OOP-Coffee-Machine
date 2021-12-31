f510500ce4d83f7e21cc62d3311ab282ef03a682
jpretty01 <jeremypretty10@gmail.com>
2021-12-31 10:04:24 -0600

    Update
    Final Update to Money Machine

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+# Import all the required files
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# Created Object to store MoneyMachine variables
+my_money_machine = MoneyMachine()
+my_coffee_maker = CoffeeMaker()
+menu = Menu()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}: ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        my_coffee_maker.report()
+        my_money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if my_coffee_maker.is_resource_sufficient(drink) and my_money_machine.make_payment(drink.cost):
+                my_coffee_maker.make_coffee(drink)
+
+
